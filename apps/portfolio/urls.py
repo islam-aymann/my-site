723a9cfd4feeaf6cba0bb57fb39b4920877f61cf
Islam Ayman <e-i.mohammed@lean.sa>
2021-09-24 23:34:18 +0200

    Add redirection on index url

--- apps/portfolio/urls.py
+++ apps/portfolio/urls.py
@@ -1,10 +1,11 @@
 from django.urls import include, path
 
-from apps.portfolio.views import index
+from apps.portfolio.views import index, RedirectionView
 
 app_name = "portfolio"
 
 urlpatterns = [
     path("", index, name="index"),
+    path("<str:destination>", RedirectionView.as_view(), name="redirection"),
     path("api/v1/", include("apps.portfolio.api.v1.urls", namespace="api_v1")),
 ]
--- apps/portfolio/views.py
+++ apps/portfolio/views.py
@@ -1,5 +1,23 @@
-from django.shortcuts import render
+from django.http import HttpRequest
+from django.shortcuts import render, redirect
+from django.views import View
 
 
 def index(request):
     return render(request, "portfolio/under-maintenance.html", {})
+
+
+class RedirectionView(View):
+    def get(self, request: HttpRequest, destination: str):
+        mapper = {
+            "linkedin": "https://www.linkedin.com/in/islam-ayman-176495106/",
+            "github": "https://github.com/islamusayman95",
+            "twitter": "https://twitter.com/islam_ayman95",
+            "instagram": "https://www.instagram.com/islamusayman95/",
+            "whatsapp": "https://wa.me/+201003346334",
+        }
+
+        return redirect(
+            mapper.get(destination, "/"),
+            permanent=True,
+        )
