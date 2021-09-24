3ace590cf04b04a618e98603849d4ebd8ad9ae93
Islam Ayman <e-i.mohammed@lean.sa>
2021-09-24 23:10:21 +0200

    Add stay tuned index page

--- tests/test_apps/test_core/test_views/test_index.py
+++ tests/test_apps/test_core/test_views/test_index.py
@@ -1,12 +1,10 @@
-import pytest
-from apps.core.models import URL
 from django.test import Client
 from django.urls import reverse
 from rest_framework import status
 
 
 def test_redirection(client: Client):
-    path = reverse("por")
+    path = reverse("portfolio:index")
     response = client.get(path)
 
-    assert response.status_code == status.HTTP_301_MOVED_PERMANENTLY
+    assert response.status_code == status.HTTP_200_OK
